367b25208dbe84f664bb84cc6fb69019d82121f8
guanqian <992678108@qq.com>
2017-12-21 16:45:47 +0800

    20171221

--- index.js
+++ index.js
@@ -3,12 +3,15 @@
 /**
  * Created by Administrator on 2017/5/10.
  */
-
+//require('jquery');
 require ('angular');
 require('angular-ui-router');
+require('ng-file-upload');
 require('angular-ui-load');
 require('oclazyload');
-var iasApp=angular.module('iasApp',['ng','ui.load','ui.router','oc.lazyLoad']);
+var iasApp=angular.module('iasApp',['ng','ui.load','ui.router','oc.lazyLoad','ngFileUpload']);
+//'ui.load', 'ui.router', 'ngStorage', 'brantwills.paging', 'oc.lazyLoad', 'fullPage.js', 'ngFileUpload'
+require('./app/menus/iasCtrl.js');
 require ('./app/menus/route.js')(iasApp)
-//require ('./app/menus/myDirective.js')
+require ('./app/menus/myDirective.js')
 require ('./app/css/index.css')
